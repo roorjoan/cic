7d191f69aa1ef7e920ad45216c50dfb1a3c242ce
José A Rodríguez <roorjoan@gmail.com>
2023-12-23 09:33:29 -0600

    cambios para agigpt-qt4

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 function calcularInteresCompuesto() {
     const inversionInicial = parseFloat(document.getElementById('inversionInicial').value);
     const porcentajeDiario = parseFloat(document.getElementById('porcentajeDiario').value);
-    const periodoDias = parseInt(document.getElementById('periodoDias').value);
+    const numeroCuantificacion = parseInt(document.getElementById('periodoDias').value);
 
     let inversionDiaria = inversionInicial;
     let gananciaTotal = 0;
+    let dia = 1;
 
     let resultadoHTML = '<h3>Resultados:</h3>';
     resultadoHTML += '<table>';
-    resultadoHTML += '<tr><th>Día</th><th>Inversión Diaria</th><th>Ganancia Diaria</th><th>Ganancia Acumulada</th></tr>';
+    resultadoHTML += '<tr><th>Día</th><th>Cuantificación</th><th>Inversión Diaria</th><th>Ganancia Diaria</th><th>Ganancia Acumulada</th></tr>';
 
-    for (let dia = 1; dia <= periodoDias; dia++) {
+    for (let cuantificacion = 1; cuantificacion <= numeroCuantificacion * 2; cuantificacion++) {
         const gananciaDiaria = (inversionDiaria * porcentajeDiario) / 100;
         gananciaTotal += gananciaDiaria;
 
-        resultadoHTML += `<tr><td>${dia}</td><td>${inversionDiaria.toFixed(3)}</td><td>${gananciaDiaria.toFixed(3)}</td><td>${gananciaTotal.toFixed(2)}</td></tr>`;
-
+        if (cuantificacion % 2 == 1) {
+            resultadoHTML += `<tr><td>${dia++}</td><td>${cuantificacion}</td><td>${inversionDiaria.toFixed(3)}</td><td>${gananciaDiaria.toFixed(3)}</td><td>${gananciaTotal.toFixed(2)}</td></tr>`;
+        } else {
+            resultadoHTML += `<tr><td></td><td>${cuantificacion}</td><td>${inversionDiaria.toFixed(3)}</td><td>${gananciaDiaria.toFixed(3)}</td><td>${gananciaTotal.toFixed(2)}</td></tr>`;
+        }
+       
         inversionDiaria += gananciaDiaria;
     }
 
